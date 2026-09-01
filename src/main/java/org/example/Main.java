4333d3e8cc63030dce24f40be5dac8f82d8dd365
NICOLE ISABEL HUALLPA COARITE <nicole.coarite@senaisp.edu.br>
2026-09-01 14:00:17 -0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void main() {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Qual é a sua idade? ");
+        int idade = entrada.nextInt();
+
+        if (idade >= 18){
+            System.out.println("Maior de idade");
+        }else {
+            System.out.println("Menor de idade");
+        }
+
+    }
+}
